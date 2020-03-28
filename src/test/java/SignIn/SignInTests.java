772414cd153508e51f437b87c50e93c498eb5423
emrikygithub <emriky@gmail.com>
2020-03-28 21:55:58 +0100

    push first code commit

---
+++ src/test/java/SignIn/SignInTests.java
@@ -0,0 +1,35 @@
+package SignIn;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class SignInTests {
+    private WebDriver driver;
+
+    public void setUp() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+
+        driver.get("https://github.com/login");
+
+        Thread.sleep(5000);
+
+        driver.manage().window().maximize();
+
+        System.out.println(driver.getTitle());
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.findElement(By.name("login")).sendKeys("emriky@gmail.com");
+        driver.findElement(By.name("password")).sendKeys("boxedup3002");
+        driver.findElement(By.name("commit")).click();
+    }
+    public static void main(String[] args) throws InterruptedException {
+        SignInTests test = new SignInTests();
+        test.setUp();
+
+    }
+}
